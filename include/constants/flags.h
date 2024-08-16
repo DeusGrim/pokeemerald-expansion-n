800631ea4c1bd095ee3f592ea826d27d5bc8f077
DeusGrim <51309181+DeusGrim@users.noreply.github.com>
2024-08-16 11:01:28 -0400

    Changes Made
    modified:   data/maps/LittlerootTown/scripts.inc
    modified:   include/config/battle.h
    modified:   include/constants/flags.h
    modified:   src/overworld.c

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -48,8 +48,8 @@
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
-#define FLAG_UNUSED_0x027    0x27 // Unused Flag
+#define FLAG_TERA_ORB_CHARGED  0x26 // Unused Flag
+#define FLAG_DYNAMAX_BATTLE    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag
